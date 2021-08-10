e2ee12e656a07accbb8c2ef7624030e0fc2f6fdb
Nicolas antonio Moncada gonzalez <nmoncada@c3r2s3.42urduliz.com>
2021-08-10 20:02:59 +0200

    1008212003100821200310082120031008212003100821200310082120031008212003100821200310082120031008212003

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,13 +6,36 @@
 /*   By: nmoncada <nmoncada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 19:29:01 by nmoncada          #+#    #+#             */
-/*   Updated: 2021/08/09 19:47:28 by nmoncada         ###   ########.fr       */
+/*   Updated: 2021/08/10 19:47:58 by nmoncada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+/*len son los caracteres que se buscan en haystack y needle es lo que busca*/
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+	size_t	n;
+	size_t	c;
+	size_t	m;
 
+	n = ft_strlen((char *)needle);
+	c = 0;
+	m = 0;
+	if (n == '\0')
+		return ((char *)haystack);
+	if (c != len)
+	{
+		while (haystack[c] != needle[m])
+		{
+			if (haystack[c] == needle[m])
+			{
+				c++;
+				m++;
+			}
+			c++;
+		}
+		return ((char *)haystack[c]);
+	}
+	else
+		return (NULL);
 }
