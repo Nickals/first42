7948b9f18c29792337c7210969d5f66b68e8e572
root <root@LAPTOP-OIO0H7QN.localdomain>
2021-08-15 20:29:25 +0200

    150821 2030 1st part

--- libft/libft.h
+++ libft/libft.h
@@ -41,6 +41,8 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
 
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
